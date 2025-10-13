9d997d1ae5474a00deff1083a30c7f0577a4e151
Shraddha <sc0171573@gmail.com>
2025-10-14 00:23:39 +0530

    Save current changes before merging signup/login

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { MyContextProvider } from "./context/myContext"; // only this provider
 import "./index.css";
-import MyState from "./context/myState"; // make sure path is correct
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MyState>
+    <MyContextProvider>
       <App />
-    </MyState>
+    </MyContextProvider>
   </React.StrictMode>
 );
